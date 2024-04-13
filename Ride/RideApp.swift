07cd5130fdcf8e756ac068c9e20d30be8ce6e935
Derek Chai <chderek9@gmail.com>
2024-04-13 12:17:31 +1200

    Initial Commit

---
+++ Ride/RideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideApp.swift
+//  Ride
+//
+//  Created by Derek Chai on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
